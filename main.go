8317b155194213f603d8a8d2e164a4bb74dc8d9e
Amal Mohanan <pillai_amal@Amals-MacBook-Air.local>
2022-06-12 17:29:40 +0530

    added custom routing bear bones

--- main.go
+++ main.go
@@ -5,13 +5,33 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Hello Welcome to amal website! </h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1> Hello Welcome!! </h1>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact me</h1>")
+}
+
+func notFounError(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Error</h1>")
+}
+
+func urlHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		homeHandler(w, r)
+	} else if r.URL.Path == "/contact" {
+		contactHandler(w, r)
+	} else {
+		notFounError(w, r)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-
+	http.HandleFunc("/", urlHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Listening on server :3000")
 	http.ListenAndServe(":3000", nil)
 }
