87fda67a60b756432af7cf2b2383c68e8278f247
pillai_amal <mail.vortextv@gmail.com>
2022-06-12 21:35:37 +0530

    cleaned up a bit

--- main.go
+++ main.go
@@ -17,18 +17,19 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact me</h1>")
 }
 
-func urlHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		homeHandler(w, r)
-	} else if r.URL.Path == "/contact" {
-		contactHandler(w, r)
-	} else {
-		http.Error(w, "404 Not Found", http.StatusNotFound) //use the default http.Error of go
-	}
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<p>Hello ask me qestions</p>")
 }
 
 func main() {
 	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Listening on server :7000")
 	http.ListenAndServe(":7000", r)
 }
