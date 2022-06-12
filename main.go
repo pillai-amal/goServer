2993b3b6741be265362af229023cb4c3b0b9aa8d
Amal Mohanan <pillai_amal@Amals-MacBook-Air.local>
2022-06-12 13:41:04 +0530

    slight alignment

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1> Hello Welcome! </h1>")
+	fmt.Fprint(w, "<h1> Hello Welcome to amal website! </h1>")
 }
 
 func main() {
 	http.HandleFunc("/", handlerFunc)
+
 	fmt.Println("Listening on server :3000")
 	http.ListenAndServe(":3000", nil)
 }
